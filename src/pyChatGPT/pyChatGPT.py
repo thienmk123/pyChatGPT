a983efbc04e1399a45ebd374ce0b8314b5c38b36
thienmk123 <virusser1109@gmail.com>
2023-01-10 00:47:35 -0400

    new options

--- src/pyChatGPT/pyChatGPT.py
+++ src/pyChatGPT/pyChatGPT.py
@@ -175,6 +175,7 @@ class ChatGPT:
         options = uc.ChromeOptions()
         options.add_argument('--window-size=1024,768')
         options.add_argument('--no-sandbox')  
+        options.add_argument('--disable-dev-shm-usage')  
         if self.__proxy:
             options.add_argument(f'--proxy-server={self.__proxy}')
         for arg in self.__chrome_args:
